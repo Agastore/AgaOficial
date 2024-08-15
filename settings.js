c3a77cbbbb186ec2f1da9eab26b1c78ad041d767
Agastore <mohanggakurniawanf12@gmail.com>
2024-08-15 18:47:46 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['62877176823','6283861585742']
+global.botname = 'Aga Oficial'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Aga Official"
+global.sticker2 = "🗿"

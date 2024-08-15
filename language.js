799f8f8723e0111b4b349fb89617af3a8485a184
Agastore <mohanggakurniawanf12@gmail.com>
2024-08-15 18:53:27 +0700

    lib

---
+++ language.js
@@ -0,0 +1,5 @@
+global.nocreator = "gak boleh, kamu bukan user vip 😠"
+global.notext = 'sertakan text ny kak 🙄'
+global.noingroup = 'Fiturnya Khusus di dalam group 😡'
+global.nobotadmin = 'Fitur ny kebuka kalo aku udah admin 😭'
+global.usernoadmin = 'Fiturnya Kebuka kalo kamu admin kak 😘'
